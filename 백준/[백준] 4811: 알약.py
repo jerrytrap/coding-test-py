668f8a0a04c5a74d5a2e577d0938bdbd5431cd40
류대현 <eogus1027@naver.com>
2025-12-20 15:31:44 +0900

    코드 수정 #64

--- 백준/[백준] 4811: 알약.py
+++ 백준/[백준] 4811: 알약.py
@@ -1,17 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-c = [0 for _ in range(31)]
-c[0] = 1
+dp = [[0 for _ in range(31)] for _ in range(31)]
 
-for n in range(1, 31):
-    total = 0
-    for i in range(n):
-        total += c[i] * c[n - 1 - i]
-    c[n] = total
+# 한 조각이 없으면 반 조각을 먹는 방법 1가지만 존재
+for h in range(31):
+    dp[0][h] = 1
+
+for w in range(1, 31):
+    for h in range(30):
+        # 반 조각이 없으면 한 조각을 반으로 쪼개 먹음
+        if h == 0:
+            dp[w][h] = dp[w - 1][h + 1]
+        # 반 조각이 있으면, 한 조각을 반으로 쪼개 먹거나 반 조각을 먹을 수 있음
+        else:
+            dp[w][h] = dp[w - 1][h + 1] + dp[w][h - 1]
 
 while True:
     n = int(input())
     if n == 0:
         break
-    print(c[n])
+
+    # 한 조각 n개, 반 조각 0개로 시작
+    print(dp[n][0])
