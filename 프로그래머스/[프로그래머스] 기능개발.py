9aa9337746b547e0d3a3db7c19c8b69fc8f525ea
류대현 <eogus1027@naver.com>
2026-02-24 17:27:50 +0900

    큐의 기능을 더 활용하도록 코드 수정 #3

--- 프로그래머스/[프로그래머스] 기능개발.py
+++ 프로그래머스/[프로그래머스] 기능개발.py
@@ -1,27 +1,24 @@
+from math import ceil
+from collections import deque
+
 def solution(progresses, speeds):
-    days = []  # 기능별 남은 일수
-    queue = []
+    queue = deque()
     answer = []
 
     # 기능별 남은 일수 계산
     for idx, progress in enumerate(progresses):
-        day = (100 - progress) // speeds[idx]
+        left = ceil((100 - progress) / speeds[idx])
+        queue.append(left)
 
-        if progress + (day * speeds[idx]) < 100:
-            day += 1
-        days.append(day)
+    while queue:
+        left = queue.popleft()
+        count = 1 # 한 번에 배포되는 기능의 개수
 
-    # 남은 일수를 기준으로 언제 배포되는지 계산
-    queue.append(days[0])
-    for i in range(1, len(days)):
-        if queue[0] >= days[i]:
-            queue.append(days[i])
-        else:
-            answer.append(len(queue))
-            queue.clear()
-            queue.append(days[i])
+        # 뒤에 있는 기능이 앞의 기능보다 먼저 끝나는 경우엔 같이 배포되므로 추가로 큐에서 꺼내줌
+        while queue and left >= queue[0]:
+            queue.popleft()
+            count += 1
 
-    # 반복이 끝난 뒤 큐에 작업할 기능이 아직 남아있다면 추가
-    if len(queue) > 0:
-        answer.append(len(queue))
+        answer.append(count)
+
     return answer
